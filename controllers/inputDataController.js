80292e7428c156e828f00e502badb9350aebcdcf
tze10 <developer@tze10.xyz>
2021-12-06 14:38:10 +0530

    working on file upload

--- controllers/inputDataController.js
+++ controllers/inputDataController.js
@@ -1,15 +1,20 @@
-const DataCollector = require('../models/data');
+const DataCollector = require("../models/data");
+const dataCollector = new DataCollector();
 
-exports.getIndexPage = (req,res,next)=>{
-   const dataCollector = new DataCollector();
-    dataCollector.getIndexData((result) =>{
-        res.render('index',{result: result});
-    })
-}
+exports.getIndexPage = (req, res, next) => {
+  dataCollector.getIndexData((result) => {
+    res.render("index", { result: result });
+  });
+};
 
-exports.postFormData = (req,res,next)=>{
-
-    const email = req.body.email;
-    console.log(email);
-    res.redirect('/');
-}
+exports.postFormData = (req, res, next) => {
+  const rollno = req.body.rollNo;
+  const name = req.body.firstName;
+  const dob = req.body.dob;
+  const address = req.body.address;
+  const email = req.body.email;
+  const imageUpload = req.body.imgUpload;
+  console.log(imageUpload);
+  dataCollector.postIdCardData(rollno, name, dob, address, email);
+  res.redirect("/");
+};
--- models/data.js
+++ models/data.js
@@ -1,6 +1,23 @@
+const sequelize = require("../util/database");
+const IdCard = require("../models/id_card");
+
 module.exports = class DataCollector {
-    constructor(){}
-    getIndexData(cb){
-        cb('this return index data');
-    }
-}
+  constructor() {
+  }
+  getIndexData(cb) {
+    cb("this return index data");
+  }
+
+  postIdCardData(rollno, name, dob, address, email) {
+    // IdCard.create({
+    //   college_id: 1001,
+    //   roll_no: rollno,
+    //   name: name,
+    //   dob: dob,
+    //   address: address,
+    //   photo: "testing",
+    //   designation: "testing",
+    //   blood_group: "testing",
+    // });
+  }
+};
